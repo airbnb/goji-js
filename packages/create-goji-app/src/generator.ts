83c21de5206f1260c6d88f78c3a97d34a6f7ae5d
Malash <i@malash.me>
2020-07-06 16:37:01 +0800

    add create-goji-app CLI

---
+++ packages/create-goji-app/src/generator.ts
@@ -0,0 +1,26 @@
+import mkdirp from 'mkdirp';
+import globby from 'globby';
+import path from 'path';
+import ejs from 'ejs';
+import fs from 'fs';
+import { promisify } from 'util';
+
+const renderTemplate = async (sourceFile: string, destFile: string, data: any = {}) => {
+  const content = await promisify(fs.readFile)(sourceFile);
+  const result = ejs.render(content.toString(), data, {
+    filename: sourceFile,
+  });
+  await mkdirp(path.dirname(destFile));
+  await promisify(fs.writeFile)(destFile, result);
+};
+
+export const generateProject = async (projectName, sourcePath: string, destPath: string) => {
+  for await (const sourceFileBuf of globby.stream(path.join(sourcePath, '**'))) {
+    const sourceFile = sourceFileBuf.toString();
+    const relativePath = path.relative(sourcePath, sourceFile);
+    const deskFile = path.join(destPath, relativePath);
+    await renderTemplate(sourceFile, deskFile, {
+      projectName,
+    });
+  }
+};
