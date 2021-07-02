0e3b6f2a6a17354a0922a3a9fe2cf9af25d6e934
Malash <i@malash.me>
2021-07-03 00:50:25 +0800

    add test cases

--- packages/webpack-plugin/src/templates/commons/__tests__/wxmlElement.test.ts
+++ packages/webpack-plugin/src/templates/commons/__tests__/wxmlElement.test.ts
@@ -1,4 +1,10 @@
-import { getComponentTagName, getConditionFromSidOrName } from '../wxmlElement';
+import { t } from '../../helpers/t';
+import {
+  element,
+  getComponentTagName,
+  getConditionFromSidOrName,
+  getEventName,
+} from '../wxmlElement';
 
 describe('getConditionFromSidOrName', () => {
   test('only name', () => {
@@ -17,5 +23,38 @@ describe('getComponentTagName', () => {
 
   test('wrapped component', () => {
     expect(getComponentTagName({ name: 'view', isWrapped: true })).toBe('goji-view');
+  });
+});
+
+describe('getEventName', () => {
+  test('alipay', () => {
+    expect(getEventName({ target: 'alipay', event: 'touchstart' })).toBe('onTouchstart');
+  });
+
+  test('other targets', () => {
+    expect(getEventName({ target: 'wechat', event: 'touchstart' })).toBe('bindtouchstart');
+    expect(getEventName({ target: 'baidu', event: 'touchstart' })).toBe('bindtouchstart');
+    expect(getEventName({ target: 'qq', event: 'touchstart' })).toBe('bindtouchstart');
+    expect(getEventName({ target: 'toutiao', event: 'touchstart' })).toBe('bindtouchstart');
+  });
+});
+
+describe('element', () => {
+  test('with children', () => {
+    expect(element({ tagName: 'view', attributes: ['class="cls"'], children: '<input />' })).toBe(t`
+      <view
+        class="cls"
+      >
+        <input />
+      </view>
+    `);
+  });
+
+  test('without children', () => {
+    expect(element({ tagName: 'view', attributes: ['class="cls"'], children: '' })).toBe(t`
+      <view
+        class="cls"
+      />
+    `);
   });
 });
