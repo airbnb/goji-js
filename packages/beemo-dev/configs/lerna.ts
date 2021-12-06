0b1a7e3c94818a8d3313fd78c6d0bce91f5cc3d4
Malash <i@malash.me>
2021-12-06 10:45:22 +0800

    release 1.0.0-alpha.16

--- packages/beemo-dev/configs/lerna.ts
+++ packages/beemo-dev/configs/lerna.ts
@@ -4,7 +4,7 @@ const config: LernaConfig = {
   packages: ['packages/*', 'packages/goji.js.org/*'],
   npmClient: 'yarn',
   useWorkspaces: true,
-  version: '1.0.0-alpha.15',
+  version: '1.0.0-alpha.16',
 };
 
 export default config;
